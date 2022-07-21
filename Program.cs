19a821575293d92d99903c4537f9501e6dc4be34
Ivan Malkovskii <103892616+gotikym@users.noreply.github.com>
2022-07-21 15:00:20 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,46 +1,34 @@
-﻿using System;
+using System;
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Вывод после конвертации " + ConvertToInt().ToString());
+        Console.WriteLine("Вывод после конвертации " + GetNumber().ToString());
     }
 
-    static int ConvertToInt()
+    static int GetNumber()
     {
-        Console.WriteLine("Введите число: ");
-        string numberUser = Console.ReadLine();
-        bool isParse;
+        bool isParse = false;
         int numberForReturn = 0;
 
-        if (isParse = int.TryParse(numberUser, out int number))
+        while (isParse == false)
         {
-            Console.WriteLine("Ваше число преобразовано");
+            Console.WriteLine("Введите число: ");
+            string numberUser = Console.ReadLine();
+
+            if (isParse = int.TryParse(numberUser, out int number))
+            {
+                Console.WriteLine("Ваше число преобразовано");
+            }
+
+            else
+            {
+                Console.WriteLine("Преобразование не удалось. Попробуйте ввести число без точек, запятых, скобочек и букв");
+            }
+
             numberForReturn = number;
         }
 
-        else
-        {
-            Console.WriteLine("Преобразование не удалось. Попробуйте ввести число без точек, запятых, скобочек и букв");
-
-            while (!isParse)
-            {
-                numberUser = Console.ReadLine();
-
-                if(isParse = int.TryParse(numberUser, out int number1))
-                {
-                    Console.WriteLine("Ваше число преобразовано");
-                }
-
-                else
-                {
-                    Console.WriteLine("Преобразование не удалось. Попробуйте ввести число без точек, запятых, скобочек и букв");
-                }
-
-                numberForReturn = number1;
-            }
-            
-        }
         return numberForReturn;
     }
 }
